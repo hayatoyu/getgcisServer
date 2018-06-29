5ddb3e8c27a66f69020ffc8e29857ab6f549dc20
hayatoyu <f19870421@gmail.com>
2018-06-29 15:32:24 +0800

    改用https

--- getgcisServer/Server.cs
+++ getgcisServer/Server.cs
@@ -286,7 +286,7 @@ namespace getGcisServer
 
                             comName = comRequest.comList[index];
                             stbr.Clear();
-                            stbr.Append("http://").Append("data.gcis.nat.gov.tw")
+                            stbr.Append("https://").Append("data.gcis.nat.gov.tw")
                                 .Append("/od/data/api/6BBA2268-1367-4B42-9CCA-BC17499EBE8C")
                                 .Append("?$format=json&$filter=")
                                 .Append(param.Replace("comName", comName));
